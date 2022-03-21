06c0ae349455b521065d4b37ecb999a52d1a2641
tumtpk <pk.prateep@gmail.com>
2022-03-22 00:18:00 +0700

    add mangum

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mangum import Mangum
 from routes.person_route import person_api_router
 from routes.students_route import students_api_router
 from routes.todos_route import todo_api_router
@@ -7,3 +8,5 @@ app = FastAPI()
 app.include_router(person_api_router)
 app.include_router(students_api_router)
 app.include_router(todo_api_router)
+
+handler = Mangum(app)
